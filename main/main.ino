a5cc10804c3ccda27c63424f3e17d84feb896009
luizguilhermefr <luizguilhermefr@gmail.com>
2018-02-09 11:10:42 -0200

    ok?

--- main/main.ino
+++ main/main.ino
@@ -52,9 +52,11 @@ SoftwareSerial BTSerial(10, 11); // RX | TX
 
 #define OP_UPDATE_UPPER_THRESHOLD 'U'
 
-const char* sensors[MAX_SENSOR]= {"00","01","02","03"};
+const int sensors[MAX_SENSOR]= {0,1,2,3};
 
-char command[RESPONSE + 1];
+char sensor[SENSOR_LENGTH];
+
+char command[RESPONSE_LENGTH + 1];
 
 float value;
 
@@ -74,17 +76,19 @@ void loop()
     for (int i = 0; i < RESPONSE_LENGTH; i++){
       command[i]= BTSerial.read();
     }
-    command[RESPONSE]= '\0';
+    command[RESPONSE_LENGTH]= '\0';
     // TODO something with incoming command
   }
 
-  for (i = 0; i < MAX_SENSOR; i++) {
+  for (int i = 0; i < MAX_SENSOR; i++) {
     response[0] = OK;
     response[STATUS_LENGTH] = OP_UPDATE_SENSOR;
-    response[STATUS_LENGTH  + OPERATION_LENGTH] = sensors[i];
+    sprintf(sensor, "%02d", sensors[i]);
+    for (int j = 0; j < SENSOR_LENGTH; j++) {
+      response[STATUS_LENGTH + OPERATION_LENGTH + j] = sensor[j];
+    }
     value = random(0, 500) / 100;
     dtostrf(value, INTEGER_LENGTH + DECIMALS_LENGTH + 1, DECIMALS_LENGTH, &response[STATUS_LENGTH + OPERATION_LENGTH + SENSOR_LENGTH]);
-
     for (int j = 0; j < RESPONSE_LENGTH; j++){
       BTSerial.write(response[j]);
     }
